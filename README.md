6ec4de8519c2a69e990390b2e700db14f81873ce
unknown <tie608400@tisnt.tis.co.jp>
2019-05-07 13:38:17 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Mendix-YubinbangoWidget
-The Yubinbango(postalcode/zipcode) widghet in Japan for Mendix
+
+The **Yubinbango widget** can be used to enter address from yubinbango/postalcode/zipcode in Japan.
+
+This module use this library (https://ajaxzip3.github.io/), and search address from zipcode on the client side only.
+
+Great thanks to ajaxzip3 (https://github.com/ajaxzip3).
+
+
+## Dependencies
+* none
+
+
+## Installation
+* Import the module **Yubinbango widget** in your project (from the Mendix AppStore or by downloading and exporting the module from this project)
+
+
+## Configuration
+- You have to create two text boxes except this widget, one is for Zip code and the other one is for Address.
+- Set those text box names into this widget properties.
+
+
+## License
+Licensed under the Apache license.
+
+
+## Version history
+0.1 first functions for YubinbangoWidget implemented
---
+++ YubinbangoWidget/widget/YubinbangoWidget.js
@@ -0,0 +1,53 @@
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+
+    "dojo/_base/lang",
+    "YubinbangoWidget/lib/jquery-1.11.2"
+
+
+], function (declare, _WidgetBase, lang, _jQuery) {
+    "use strict";
+
+    var $ = _jQuery.noConflict(true);
+
+    return declare("YubinbangoWidget.widget.YubinbangoWidget", [ _WidgetBase ], {
+
+
+        // Internal variables.
+        _handles: null,
+        _contextObj: null,
+
+        constructor: function () {
+            this._handles = [];
+        },
+
+        postCreate: function () {
+            logger.debug(this.id + ".postCreate");
+            this._insertYubin();
+
+            $('.mx-name-' + this.address1 + ' input').attr('name', this.address1);
+            $('.mx-name-' + this.zipcode1 + ' input').attr('name', this.zipcode1);
+            $('.mx-name-' + this.zipcode1 + ' input').attr('onkeyup', 'AjaxZip3.zip2addr("' + this.zipcode1 + '", "", "' + this.address1 + '", "' + this.address1 + '");');
+        },
+
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
+
+            this._contextObj = obj;
+            callback();
+        },
+
+        _insertYubin: function () {
+            logger.debug(this.id + "._insertYubin");
+            var a = document.createElement("script");
+            var m = document.getElementsByTagName("script")[0];
+            a.src = "https://ajaxzip3.github.io/ajaxzip3.js";
+            a.charset = "UTF-8";
+            m.parentNode.insertBefore(a, m);
+        },
+
+    });
+});
+
+require(["YubinbangoWidget/widget/YubinbangoWidget"]);
